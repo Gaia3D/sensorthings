850a194def5e0b2cb4a4a43e6d04bd133f015711
정연화 <yhjeong@gaia3d.com>
2021-05-25 22:50:24 +0900

    refactor : 불필요한 코드 제거

--- src/main/java/gaia3d/config/RootConfig.java
+++ src/main/java/gaia3d/config/RootConfig.java
@@ -3,30 +3,14 @@ package gaia3d.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
 public class RootConfig {
 
     @Bean
-    public RestTemplate airkoreaRestTemplate(PropertiesConfig propertiesConfig, RestTemplateBuilder restTemplateBuilder) {
-
-        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
-        messageConverters.add(new Jaxb2RootElementHttpMessageConverter());
-        messageConverters.add(new MappingJackson2HttpMessageConverter());
-
-        return restTemplateBuilder
-                //.messageConverters(messageConverters)
-                //.messageConverters(new MappingJackson2HttpMessageConverter())
-                .build();
+    public RestTemplate airkoreaRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
     }
 
     @Bean
